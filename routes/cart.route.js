ef0386b9ff2fedc1131a1c8c5ba86bf7c2100570
nandan-wewhare <nandanwewhare25@gmail.com>
2022-10-05 16:25:03 +0530

    updated add to cart endpoint to match REST arch.

--- routes/cart.route.js
+++ routes/cart.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cartRouter = express.Router();
 const cartController = require("../controllers/cart.controller");
 
-cartRouter.post("/add/:userId", cartController.addItemToCart);
+cartRouter.post("/:userId", cartController.addItemToCart);
 cartRouter.get("/:userId", cartController.getCart);
 cartRouter.patch("/:userId", cartController.decreaseQuantity);
 cartRouter.delete("/:userId", cartController.removeItem);
